e4ffa0a4b5aa86705830162858cdb92b293d028e
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-08-25 18:25:56 +0900

    export * from 이 작동하지 않아 수정
    export * from ... 으로 작성했을 때 import와 export가 동시에 되지 않아
    import export문을 따로 작성하였습니다

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
-export * from "./Home/Home";
-export * from "./Todo/Todo";
-export * from "./SignIn/SignIn";
-export * from "./SignUp/SignUp";
-export * from "./Error/Error";
+import Home from "./Home/Home";
+import Todo from "./Todo/Todo";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import Error from "./Error/Error";
+
+export { Home, Todo, SignIn, SignUp, Error };
