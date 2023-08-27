97acf6d6faec8ff21cdff8ae3cdf6df6c11cff43
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-08-27 17:49:23 +0900

    SignIn.tsx 전체 html과 css구현

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,5 +1,47 @@
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="bg-white w-96 rounded-md shadow-md">
+      <h1 className="text-xl text-white p-4 bg-gray-900">로그인</h1>
+      <form className="flex flex-col gap-2 p-4">
+        <div>
+          <label className="text-sm" htmlFor="email">
+            아이디
+          </label>
+          <input
+            className="w-full py-4 px-2 border border-slate-200 h-12"
+            type="email"
+            id="email"
+          />
+          <div className="mt-4 font-medium text-xs"></div>
+        </div>
+        <div>
+          <label className="text-sm" htmlFor="password">
+            비밀번호
+          </label>
+          <input
+            className="w-full py-4 px-2 border border-slate-200 h-12"
+            type="password"
+            id="password"
+          />
+          <div className="mt-4 font-medium text-xs"></div>
+        </div>
+        <button
+          className="bg-gray-900 h-10 rounded-lg text-white disabled:opacity-30"
+          type="submit"
+        >
+          로그인
+        </button>
+      </form>
+      <div className="text-center py-4 text-xs">
+        <span>아직 계정이 없나요? </span>
+        <Link to="/signup" className="text-blue-500 font-semibold">
+          회원가입하기
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
