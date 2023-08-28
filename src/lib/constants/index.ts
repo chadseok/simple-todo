df4fdcff12948c71764b2d929d37db4d2c3c5613
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-08-28 17:53:14 +0900

    storage.ts -> token.ts로 변경
    storage안의 key:value쌍은 storage에 묶여있는 것이 아니므로
    storage라는 카테고리는 삭제하고 간단하게 token만 모아 두도록
    파일명을 token으로 변경하였습니다

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,2 +1,2 @@
 export * from "./path";
-export * from "./storage";
+export * from "./token";
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,16 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { USER_TOKEN, ROUTE_PATH } from "@/lib/constants";
+import { USER_AUTH_TOKEN, ROUTE_PATH } from "@/lib/constants";
 
 export const PublicRoute = () => {
   return <Outlet />;
 };
 
 export const AuthOnlyRoute = () => {
-  const token = localStorage.getItem(USER_TOKEN);
+  const token = localStorage.getItem(USER_AUTH_TOKEN);
   return token ? <Outlet /> : <Navigate to={ROUTE_PATH.SIGN_IN} />;
 };
 
 export const GuestOnlyRoute = () => {
-  const token = localStorage.getItem(USER_TOKEN);
+  const token = localStorage.getItem(USER_AUTH_TOKEN);
   return token ? <Navigate to={ROUTE_PATH.TODO} /> : <Outlet />;
 };
