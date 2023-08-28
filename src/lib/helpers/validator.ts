89e212f1469cf0fd04f8e0f8c30105fc5d324356
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-08-28 20:42:15 +0900

    email과 password 유효성 검사하는 validator객체 구현

---
+++ src/lib/helpers/validator.ts
@@ -0,0 +1,10 @@
+export const validator = {
+  checkEmail: (email: string) => {
+    return new RegExp(
+      /^[\w.!#$%&'*+/=?^`{|}~-]+@[\w.!#$%&'*+/=?^`{|}~-]+$/
+    ).test(email);
+  },
+  checkPassword: (password: string) => {
+    return password.length < 8 ? false : true;
+  },
+};
