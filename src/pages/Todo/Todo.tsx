507ad8340b6c2d1511f7436c6d6152666c5e4798
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-02 20:20:57 +0900

    Todo페이지 기능 구현

--- src/pages/Todo/Todo.tsx
+++ src/pages/Todo/Todo.tsx
@@ -1,5 +1,59 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { createTodoApi } from "@/api/todo";
+import type { TodoData } from "@/lib/types/main";
+import TodoItem from "./TodoItem";
+
 const Todo = () => {
-  return <div>Todo</div>;
+  const [todoList, setTodoList] = React.useState(useLoaderData() as TodoData[]);
+  const [todoText, setTodoText] = React.useState("");
+
+  const createTodo = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const res = await createTodoApi(todoText);
+      setTodoList([...todoList, res.data]);
+      setTodoText("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-slate-100 w-[36rem] h-[100vh] shadow-md overflow-auto">
+      <h1 className="text-xl text-white p-4 bg-gray-900">투두</h1>
+      <form
+        className="relative p-4 border-b-2 border-dotted border-slate-300"
+        onSubmit={createTodo}
+      >
+        <input
+          className="w-full h-12 bg-slate-300 rounded px-4 py-2"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+          spellCheck={false}
+        />
+        <button
+          className="absolute right-4 bg-slate-400 w-12 h-12 rounded-r hover:bg-slate-500"
+          type="submit"
+        >
+          🖊️
+        </button>
+      </form>
+      <div className="p-4">
+        <ul>
+          {todoList.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              data={todo}
+              todoList={todoList}
+              setTodoList={setTodoList}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
---
+++ src/pages/Todo/TodoItem.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { updateTodoApi, deleteTodoApi } from "@/api/todo";
+import type { TodoData } from "@/lib/types/main";
+
+type Props = {
+  data: TodoData;
+  todoList: TodoData[];
+  setTodoList: React.Dispatch<React.SetStateAction<TodoData[]>>;
+};
+
+function TodoItem(props: Props) {
+  const [todoData, setTodoData] = React.useState(props.data);
+  const [newTodo, setNewTodo] = React.useState(props.data.todo);
+  const [editMode, setEditMode] = React.useState(false);
+
+  const toggleCheckBox = async () => {
+    try {
+      const res = await updateTodoApi({
+        ...todoData,
+        isCompleted: !todoData.isCompleted,
+      });
+      setTodoData(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const enterEditMode = () => {
+    setEditMode(true);
+  };
+
+  const editTodoText = () => async () => {
+    try {
+      const res = await updateTodoApi({ ...todoData, todo: newTodo });
+      setTodoData(res.data);
+      setEditMode(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const cancelEdit = () => {
+    setNewTodo(todoData.todo);
+    setEditMode(false);
+  };
+
+  const deleteTodo = async () => {
+    try {
+      if (window.confirm("삭제하시겠습니까?")) {
+        await deleteTodoApi(todoData);
+        props.setTodoList(
+          props.todoList.filter((todo) => todo.id !== todoData.id)
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <li
+      className={`relative flex items-center h-12 p-2 mb-2 rounded ${
+        todoData.isCompleted ? "bg-slate-50" : "bg-white"
+      } hover:bg-slate-50`}
+    >
+      <input
+        className="w-4 h-4 mr-4"
+        name="todo"
+        type="checkbox"
+        onClick={toggleCheckBox}
+        defaultChecked={todoData.isCompleted}
+      />
+      {editMode ? (
+        <input
+          className="bg-transparent border-none h-8 focus:outline-none"
+          autoFocus
+          spellCheck={false}
+          value={newTodo}
+          onChange={(e) => {
+            setNewTodo(e.target.value);
+          }}
+        />
+      ) : (
+        <span
+          className={`${
+            todoData.isCompleted ? " line-through text-color-300" : ""
+          }`}
+        >
+          {todoData.todo}
+        </span>
+      )}
+      <div className="grow"></div>
+      {editMode ? (
+        <div>
+          <button className="mr-4 text-sm" onClick={cancelEdit}>
+            취소
+          </button>
+          <button className="mr-4 text-sm" onClick={editTodoText}>
+            수정
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button className="mr-4" onClick={enterEditMode}>
+            📝
+          </button>
+          <button className="mr-4" onClick={deleteTodo}>
+            🗑️
+          </button>
+        </div>
+      )}
+    </li>
+  );
+}
+
+export default TodoItem;
