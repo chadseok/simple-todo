7ab267a844dad7d94db208c1948319b841b345a8
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-09-04 17:19:35 +0900

    Router의 errorElement에 해당 하는 페이지 구성
    Route에서 일어나는 에러와 일반에러를 따로 구분하여
    표시하도록 하였습니다

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,5 +1,28 @@
-const Error = () => {
-  return <div>Error</div>;
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="text-center">
+        <h1 className="text-2xl font-bold mb-8">
+          서버 연결에 문제가 생겼습니다!
+        </h1>
+        <h2>{error.status}</h2>
+        <p>{error.data?.message}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div className="text-center">
+        <h1 className="text-2xl font-bold mb-8">
+          예상치 못한 에러가 발생했습니다!
+        </h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  }
 };
 
-export default Error;
+export default ErrorPage;
